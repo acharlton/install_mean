bf4085203bdcab4b2f871bfa0042847dffcbce1c
coderabbit <charlton@un.org>
2015-11-09 21:57:20 +0100

    added pg library

--- install_meanjs.bash
+++ install_meanjs.bash
@@ -17,14 +17,25 @@ sudo apt-get install postgresql postgresql-contrib
 sudo apt-get install postgis
 sudo apt-get install pgadmin3
 
+
+# create project folder
+mkdir myproject
+cd myproject
+express myapp
+cd myapp
+npm install
+npm install --save sequelize
+npm install --save pg pg-hstore
+
+
 # log into Postgres
 sudo -u postgres psql postgres
 
-echo "Install Mongo DB"
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
-sudo apt-get update
-sudo apt-get -y install mongodb-org
+#echo "Install Mongo DB"
+#apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+#echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+#sudo apt-get update
+#sudo apt-get -y install mongodb-org
 
 echo "Install git"
 sudo apt-get update
